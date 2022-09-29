cd4c453bba7acab8cc8d0c2e2ae4a185acfd8fd1
Egon Willighagen <egon.willighagen@gmail.com>
2022-09-29 14:06:22 +0200

    Added some metadata

--- convertToHTML.groovy
+++ convertToHTML.groovy
@@ -6,8 +6,18 @@ def termList = parser.parse(new File("blueprint_20210928.json"))
 // output (just to STDOUT for now)
 
 println """<html>
+  <head>
+    <title>GRACIOUS framework blueprint</title>
+  </head>
 """
 
+println """<body>
+<h1>GRACIOUS framework blueprint</h1>
+<p>
+This document is a HTML translation of the GRACIOUS framework blueprint (Traas, Lion, & Vanhauten, Ralph. (2021). doi:<a href="https://doi.org/10.5281/zenodo.6140453">10.5281/zenodo.6140453</a>).
+This work is part of <a href="https://www.sbd4nano.eu/"SbD4Nano</a> and has received funding from the European Union’s Horizon 2020 research and innovation program under grant agreement No. 862195.
+</p>
+"""
 // endpoints
 int counter = 0
 for (kb in termList.knowledgebases) {
